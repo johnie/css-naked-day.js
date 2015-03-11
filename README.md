41a70f77243d8671baa81baeeca9ee5b32940e63
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-03-11 13:11:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# css-naked-day.js
+
+> Make your website feel endless.
+
+## Install
+
+**Bower**
+```bash
+$ bower install css-naked-day.js
+```
+
+```javascript
+cssNakedDay.init();
+```
+
+### Licence
+
+MIT © [Johnie Hjelm](http://johnie.se)
---
+++ css-naked-day.js
@@ -0,0 +1,42 @@
+/**
+ * css-naked-day.js
+ * Copyright © 2015 Johnie Hjelm
+ */
+
+'use strict';
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+		exports.cssNakedDay = factory();
+	} else {
+    root.cssNakedDay = factory();
+  }
+}(this, function () {
+  return {
+
+    getNaked: function(date) {
+      var dateString = date;
+      var month      = (dateString.getMonth() + 1);
+      var day        = (dateString.getDate());
+
+      return (month === 4 && day === 9);
+    },
+
+    init: function(date, naked) {
+      if(!date instanceof Date) {
+        throw new Error('Invalid date format. Use new Date()');
+      }
+      date  = date || new Date();
+      naked = this.getNaked(date);
+      if ( naked ) {
+        var css = document.styleSheets;
+        for (var i = 0; i < css.length; i++) {
+          css[i].disabled = true;
+        }
+      }
+    }
+
+  };
+}));
